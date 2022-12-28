c650a37b9ac7358d72bda94d14099a2e854eed56
violog <51th.apprent1ce.f0rce@gmail.com>
2022-12-28 13:29:25 +0200

    Enhance Generator constructor and mkdir

--- src/abigen/generator.ts
+++ src/abigen/generator.ts
@@ -1,40 +1,41 @@
-import { execSync } from "child_process"
-import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { exec } from "child_process"
+import { Artifacts, HardhatRuntimeEnvironment } from "hardhat/types"
 import _ from "lodash"
 import { basename, resolve } from "path"
-import { existsSync, mkdirSync } from "fs"
-import { stat, readdir, rm } from "fs/promises"
+import { existsSync } from "fs"
+import { mkdir, stat, readdir, rm } from "fs/promises"
 
 export class Generator {
-    private pkgName: string
-    private outDir: string
-    private dirExists: boolean
     private abigenPath: string
-    // fixme: should I only pass hre.artifacts, outDir and abigenPath?
-    constructor(private hre: HardhatRuntimeEnvironment) {
-        this.outDir = resolve(hre.config.gobind.outDir)
+    private artifacts: Artifacts
+    private dirExists: boolean
+    private outDir: string
+    private pkgName: string
+
+    constructor(hre: HardhatRuntimeEnvironment) {
         this.abigenPath = resolve(hre.config.gobind.abigenPath)
+        this.artifacts = hre.artifacts
+        this.outDir = resolve(hre.config.gobind.outDir)
         this.dirExists = existsSync(this.outDir)
         this.pkgName = _.camelCase(basename(this.outDir))
     }
 
     async generateAll() {
-        const names = await this.hre.artifacts.getAllFullyQualifiedNames()
+        const names = await this.artifacts.getAllFullyQualifiedNames()
         await this.generate(names)
     }
 
     async generate(artifactNames: string[]) {
         for (const name of artifactNames) {
             if (!this.dirExists) {
-                mkdirSync(this.outDir)
+                await mkdir(this.outDir)
                 this.dirExists = true
             }
 
-            const artifact = await this.hre.artifacts.readArtifact(name)
+            const artifact = await this.artifacts.readArtifact(name)
             const abi = JSON.stringify(artifact.abi)
             const contract = artifact.contractName
-            // todo: try exec async
-            execSync(`echo '${abi}' | ${this.abigenPath} --pkg ${this.pkgName} --type ${contract} --out ${this.outDir}/${contract}.go --abi -`)
+            exec(`echo '${abi}' | ${this.abigenPath} --pkg ${this.pkgName} --type ${contract} --out ${this.outDir}/${contract}.go --abi -`)
         }
     }
 
