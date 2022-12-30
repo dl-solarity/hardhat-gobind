c00dc7e95786aa6f07118bc065a41e8704537bab
violog <51th.apprent1ce.f0rce@gmail.com>
2022-12-30 15:57:50 +0200

    Allow only relative paths for include/exclude

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
+import { NomicLabsHardhatPluginError } from "hardhat/plugins";
 import { ConfigExtender } from "hardhat/types";
+import { isAbsolute } from "path";
+import { pluginName } from "./constants";
 
 export const getDefaultGoBindConfig: ConfigExtender = (resolvedConfig, config) => {
   const defaultConfig = {
@@ -14,7 +17,14 @@ export const getDefaultGoBindConfig: ConfigExtender = (resolvedConfig, config) =
     return;
   }
 
+  if (!areRelativePaths(config.gobind.onlyFiles))
+    throw new NomicLabsHardhatPluginError(pluginName, "config.gobind.onlyFiles must only include relative paths");
+  if (!areRelativePaths(config.gobind.skipFiles))
+    throw new NomicLabsHardhatPluginError(pluginName, "config.gobind.skipFiles must only include relative paths");
+
   const { cloneDeep } = require("lodash");
   const customConfig = cloneDeep(config.gobind);
   resolvedConfig.gobind = { ...defaultConfig, ...customConfig };
 };
+
+const areRelativePaths = (array?: string[]): boolean => array === undefined || array.every((p) => !isAbsolute(p));
