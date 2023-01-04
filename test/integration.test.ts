9d430a956ec6f15eee6d32ea7d53fc638703d2c9
violog <51th.apprent1ce.f0rce@gmail.com>
2023-01-04 19:22:36 +0200

    Add minimal integration tests

---
+++ test/integration.test.ts
@@ -0,0 +1,52 @@
+import { assert } from "chai";
+import { existsSync } from "fs";
+import { TASK_CLEAN, TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
+import path from "path";
+
+import { TASK_GOBIND } from "../src/constants";
+import { useEnvironment } from "./helpers";
+
+describe("GoBind x Hardhat", function () {
+    useEnvironment("hardhat-project-undefined-config");
+
+    const assertExists = (path: string) => assert.isTrue(existsSync(path), `path ${path} should exist`);
+    const assertNotExists = (path: string) => assert.isFalse(existsSync(path), `path ${path} should not exist`);
+    const assertContractsGenerated = (outdir: string) => {
+        assertExists(outdir);
+        assertExists(`${outdir}/contracts`);
+        assertExists(`${outdir}/contracts/Lock.go`);
+    };
+
+    it("does not generate bindings with --no-compile and no artifacts", async function () {
+        const outdir = path.resolve(this.env.config.gobind.outdir);
+
+        await this.env.run(TASK_GOBIND, { noCompile: true });
+        assertNotExists(outdir);
+    });
+
+    it("compiles and generates bindings", async function () {
+        const outdir = path.resolve(this.env.config.gobind.outdir);
+        assertNotExists(outdir);
+
+        await this.env.run(TASK_GOBIND);
+        assertContractsGenerated(outdir);
+    });
+
+    it("cleans up generated bindings", async function () {
+        const outdir = path.resolve(this.env.config.gobind.outdir);
+        assertExists(outdir);
+
+        await this.env.run(TASK_CLEAN);
+        assertNotExists(outdir);
+    });
+
+    it("generates bindings on compilation with --generate-bindgings", async function () {
+        const outdir = path.resolve(this.env.config.gobind.outdir);
+        assertNotExists(outdir);
+
+        await this.env.run(TASK_COMPILE, { generateBindings: true });
+        assertContractsGenerated(outdir);
+
+        await this.env.run(TASK_CLEAN);
+    });
+});
