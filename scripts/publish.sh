29ae2b451bf745bcfadf4aba7f54c7f7e4863cd7
Kyryl R <kyryl.ryabov@gmail.com>
2023-09-04 18:31:17 +0300

    Updated package json

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-cp -r ./README.md ./LICENSE ./dist ./src ./bin ./publish
+cp -r ./package.json ./README.md ./LICENSE ./dist ./src ./bin ./publish
 npm publish ./publish/ --access public
-rm -r ./publish/README.md ./publish/LICENSE ./publish/dist ./publish/src ./publish/bin
+rm -r ./publish/package.json ./publish/README.md ./publish/LICENSE ./publish/dist ./publish/src ./publish/bin
